fbdcf09997d94284472208c343836432356bbeb1
LPespisa <LPespisa@gmail.com>
2014-03-28 11:26:40 -0400

    Cleaning up forms, still no dynamic fields, none of the people knew enough to help.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,12 +15,9 @@
 //= require foundation
 //= require timelineJS/embed
 //= require turbolinks
-//= require underscore
-//= require backbone
-//= require backbone_rails_sync
-//= require backbone_datalink
 //= require_tree .
 //= require_self.
+
 
 $(function() {
     $(document).foundation();
