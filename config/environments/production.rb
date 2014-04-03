888997dc2af2f1427f1b5a2ec83ab4873b80a1cf
LPespisa <LPespisa@gmail.com>
2014-04-02 22:11:26 -0400

    more fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Inputtest2::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -52,7 +52,7 @@ Inputtest2::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+   config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
